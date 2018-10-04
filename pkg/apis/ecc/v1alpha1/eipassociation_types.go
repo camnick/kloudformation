1cc6120ae060e6bf99a022088a7387d6b5ca56b7
cnickoloff <33676695+cnickoloff@users.noreply.github.com>
2018-10-04 10:05:17 -0700

    Forgot one field in EIPAssociation type. Changed order in examples.

--- pkg/apis/ecc/v1alpha1/eipassociation_types.go
+++ pkg/apis/ecc/v1alpha1/eipassociation_types.go
@@ -27,8 +27,8 @@ import (
 type EIPAssociationSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
-	AllocationName string `json:"allocationName"` //pull Allocated EIP name, read annotations to get AllocationId
-	InstanceName   string `json:"ec2InstanceName"`   // eull InstanceName, and get InstanceId from annotations
+	AllocationName  string `json:"allocationName"`  //pull Allocated EIP name, read annotations to get AllocationId
+	EC2InstanceName string `json:"ec2InstanceName"` // eull InstanceName, and get InstanceId from annotations
 	//AllowReassociation bool `json:"allowReassociation"`
 	//NetworkInterfaceId string `json:"networkInterfaceId"`
 	//PrivateIpAddress string `json:"privateIpAddress"`
