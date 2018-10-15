e75ba191db5a13d9b00034dbcd1f7706c51b952c
cnickoloff <33676695+cnickoloff@users.noreply.github.com>
2018-10-15 13:16:26 -0700

    create/delete aws keypair okay, create k8s secret works. no delete

--- pkg/controller/ec2keypair/ec2keypair_controller.go
+++ pkg/controller/ec2keypair/ec2keypair_controller.go
@@ -28,6 +28,8 @@ import (
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	//"k8s.io/apimachinery/pkg/types"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/tools/record"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
@@ -171,6 +173,31 @@ func (r *ReconcileEC2KeyPair) Reconcile(request reconcile.Request) (reconcile.Re
 			return reconcile.Result{}, err
 		}
 		r.events.Event(instance, `Normal`, `Annotated`, "Added finalizer and annotations")
+
+		// logic to generate kubernetes secret based off ec2keypair here
+		// define the secret
+		keySecret := &corev1.Secret{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      instance.Name + "-private-key",
+				Namespace: instance.Namespace,
+			},
+			Data: map[string][]byte{
+				"PrivateKey":  []byte(*createOutput.KeyMaterial),
+				"FingerPrint": []byte(*createOutput.KeyFingerprint),
+			},
+		}
+		// create the Secret
+		err = r.Create(context.TODO(), keySecret)
+		/*
+			if err != nil {
+				r.events.Eventf(instance, `Warning`, `CreateFailure`, "Failed to create the secret: %s", err.Error())
+				return reconcile.Result{}, err
+			}
+		*/
+
+		//keySecret.ObjectMeta.Annotations[`generatedBy`] = awsKeyName
+		keySecret.ObjectMeta.Finalizers = append(keySecret.ObjectMeta.Finalizers, `ec2keypairs.ecc.aws.gotopple.com`)
+		r.events.Event(keySecret, `Normal`, `Annotated`, "Added finalizer and annotations")
 
 	} else if instance.ObjectMeta.DeletionTimestamp != nil {
 		// remove the finalizer
