f62d371f42c9090c12f1a88aacb143248807f55e
cnickoloff <33676695+cnickoloff@users.noreply.github.com>
2018-10-25 09:20:10 -0700

    minor script addition

--- savekey.sh
+++ savekey.sh
@@ -1,1 +1,2 @@
-k get secret -o yaml  | grep PrivateKey: | awk '{print $2}' | base64 --decode > ~/.ssh/sample-keypair
+k get secret -o yaml  | grep PrivateKey: | awk '{print $2}' | base64 --decode > ~/.ssh/$1.pem
+echo "You saved the key as $1."
