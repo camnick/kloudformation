853786b25594a0672d473b0e86a5afd09edd7a4a
cnickoloff <33676695+cnickoloff@users.noreply.github.com>
2018-10-31 10:50:42 -0700

    Inital generation of swarm

---
+++ pkg/apis/swarm/v1alpha1/dockerswarm_types.go
@@ -0,0 +1,62 @@
+/*
+Copyright 2018 The KloudFormation authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// DockerSwarmSpec defines the desired state of DockerSwarm
+type DockerSwarmSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+// DockerSwarmStatus defines the observed state of DockerSwarm
+type DockerSwarmStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// DockerSwarm is the Schema for the dockerswarms API
+// +k8s:openapi-gen=true
+type DockerSwarm struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   DockerSwarmSpec   `json:"spec,omitempty"`
+	Status DockerSwarmStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// DockerSwarmList contains a list of DockerSwarm
+type DockerSwarmList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []DockerSwarm `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&DockerSwarm{}, &DockerSwarmList{})
+}
