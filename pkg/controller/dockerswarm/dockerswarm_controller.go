8322de9911b97dcaf2bd3633b770b90974ae8c6f
cnickoloff <33676695+cnickoloff@users.noreply.github.com>
2018-11-02 11:56:00 -0700

    put in some logging, still doesn't work

--- pkg/controller/dockerswarm/dockerswarm_controller.go
+++ pkg/controller/dockerswarm/dockerswarm_controller.go
@@ -20,13 +20,13 @@ import (
 	"context"
 	"fmt"
 
-	aws "github.com/aws/aws-sdk-go/aws"
+	//aws "github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	awssession "github.com/aws/aws-sdk-go/aws/session"
 	ec2 "github.com/aws/aws-sdk-go/service/ec2"
 	eccv1alpha1 "github.com/gotopple/kloudformation/pkg/apis/ecc/v1alpha1"
 	//iamv1alpha1 "github.com/gotopple/kloudformation/pkg/apis/iam/v1alpha1"
-	//swarm "github.com/gotopple/kloudformation/pkg/apis/swarm/v1alpha1"
+	swarmv1alpha1 "github.com/gotopple/kloudformation/pkg/apis/swarm/v1alpha1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -40,7 +40,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/source"
 )
 
-// Add creates a new Subnet Controller and adds it to the Manager with default RBAC. The Manager will set fields on the Controller
+// Add creates a new DockerSwarm Controller and adds it to the Manager with default RBAC. The Manager will set fields on the Controller
 // and Start it when the Manager is Started.
 func Add(mgr manager.Manager) error {
 	return add(mgr, newReconciler(mgr))
@@ -52,7 +52,7 @@ func newReconciler(mgr manager.Manager) reconcile.Reconciler {
 		SharedConfigState: awssession.SharedConfigEnable,
 	}))
 	r := mgr.GetRecorder(`dockerswarm-controller`)
-	return &ReconcileSubnet{Client: mgr.GetClient(), scheme: mgr.GetScheme(), sess: sess, events: r}
+	return &ReconcileDockerSwarm{Client: mgr.GetClient(), scheme: mgr.GetScheme(), sess: sess, events: r}
 }
 
 // add adds a new Controller to mgr with r as the reconcile.Reconciler
@@ -63,8 +63,8 @@ func add(mgr manager.Manager, r reconcile.Reconciler) error {
 		return err
 	}
 
-	// Watch for changes to Subnet
-	err = c.Watch(&source.Kind{Type: &eccv1alpha1.Subnet{}}, &handler.EnqueueRequestForObject{})
+	// Watch for changes to DockerSwarm
+	err = c.Watch(&source.Kind{Type: &swarmv1alpha1.DockerSwarm{}}, &handler.EnqueueRequestForObject{})
 	if err != nil {
 		return err
 	}
@@ -72,23 +72,23 @@ func add(mgr manager.Manager, r reconcile.Reconciler) error {
 	return nil
 }
 
-var _ reconcile.Reconciler = &ReconcileSubnet{}
+var _ reconcile.Reconciler = &ReconcileDockerSwarm{}
 
-// ReconcileSubnet reconciles a Subnet object
-type ReconcileSubnet struct {
+// ReconcileSubnet reconciles a DockerSwarm object
+type ReconcileDockerSwarm struct {
 	client.Client
 	scheme *runtime.Scheme
 	sess   *awssession.Session
 	events record.EventRecorder
 }
 
-// Reconcile reads that state of the cluster for a Subnet object and makes changes based on the state read
-// and what is in the Subnet.Spec
+// Reconcile reads that state of the cluster for a DockerSwarm object and makes changes based on the state read
+// and what is in the DockerSwarm.Spec
 // Automatically generate RBAC rules to allow the Controller to read and write Deployments
 // +kubebuilder:rbac:groups=ecc.aws.gotopple.com,resources=dockerswarms,verbs=get;list;watch;create;update;patch;delete
-func (r *ReconcileSubnet) Reconcile(request reconcile.Request) (reconcile.Result, error) {
-	// Fetch the Subnet instance
-	instance := &eccv1alpha1.Subnet{}
+func (r *ReconcileDockerSwarm) Reconcile(request reconcile.Request) (reconcile.Result, error) {
+	// Fetch the DockerSwarm instance
+	instance := &swarmv1alpha1.DockerSwarm{}
 	err := r.Get(context.TODO(), request.NamespacedName, instance)
 	if err != nil {
 		if errors.IsNotFound(err) {
@@ -106,12 +106,14 @@ func (r *ReconcileSubnet) Reconcile(request reconcile.Request) (reconcile.Result
 	// iamsvc will create iam resources.
 	ec2svc := ec2.New(r.sess)
 	//iamsvc := iam.New(r.sess)
-	// get the SubnetId out of the annotations
+	// get the DockerSwarmId out of the annotations
 	// if absent then create
 	dockerswarmid, ok := instance.ObjectMeta.Annotations[`dockerswarmid`]
+	r.events.Eventf(instance, `Normal`, `Info`, "Checking if Swarm Exists")
 	if !ok {
-
+		r.events.Eventf(instance, `Normal`, `Info`, "Swarm doesn't exist- Creating")
 		//define the vpc object
+		r.events.Eventf(instance, `Normal`, `Info`, "Defining the VPC")
 		vpc := &eccv1alpha1.VPC{
 			ObjectMeta: metav1.ObjectMeta{
 				Name: instance.Name + "-vpc",
@@ -129,43 +131,43 @@ func (r *ReconcileSubnet) Reconcile(request reconcile.Request) (reconcile.Result
 				},
 			},
 		}
-
+		r.events.Eventf(instance, `Normal`, `Info`, "Creating the Swarm's VPC")
 		err = r.Create(context.TODO(), vpc)
 		if err != nil {
-			return reconcile.Result{}, err
-		} else if err != nil {
+			r.events.Eventf(instance, `Normal`, `Info`, "VPC creation failed")
 			return reconcile.Result{}, err
 		}
+		r.events.Eventf(instance, `Normal`, `Info`, "VPC creation worked")
 
-		r.events.Eventf(instance, `Normal`, `CreateAttempt`, "Creating AWS Subnet in %s", *r.sess.Config.Region)
+		r.events.Eventf(instance, `Normal`, `CreateAttempt`, "Creating AWS DockerSwarm in %s", *r.sess.Config.Region)
 		createOutput, err := ec2svc.CreateSubnet(&ec2.CreateSubnetInput{
 			//			AssignIpv6AddressOnCreation: aws.Bool(instance.Spec.AssignIpv6AddressOnCreation),
 			//			MapPublicIpOnLaunch:         aws.Bool(instance.Spec.MapPublicIpOnLaunch),
-			AvailabilityZone: aws.String(instance.Spec.AvailabilityZone),
-			CidrBlock:        aws.String(instance.Spec.CIDRBlock),
+			//AvailabilityZone: aws.String(instance.Spec.AvailabilityZone),
+			//CidrBlock:        aws.String(instance.Spec.CIDRBlock),
 			//Ipv6CidrBlock:    aws.String(instance.Spec.IPv6CIDRBlock),
-			VpcId: aws.String(vpc.ObjectMeta.Annotations[`vpcid`]),
+			//VpcId: aws.String(vpc.ObjectMeta.Annotations[`vpcid`]),
 		})
 		if err != nil {
 			r.events.Eventf(instance, `Warning`, `CreateFailure`, "Create failed: %s", err.Error())
 			return reconcile.Result{}, err
 		}
 		if createOutput == nil {
-			return reconcile.Result{}, fmt.Errorf(`CreateSubnetOutput was nil`)
+			return reconcile.Result{}, fmt.Errorf(`CreateDockerSwarmOutput was nil`)
 		}
 
 		dockerswarmid = *createOutput.Subnet.SubnetId
-		r.events.Eventf(instance, `Normal`, `Created`, "Created AWS Subnet (%s)", dockerswarmid)
+		r.events.Eventf(instance, `Normal`, `Created`, "Created AWS DockerSwarm (%s)", dockerswarmid)
 		instance.ObjectMeta.Annotations[`dockerswarmid`] = dockerswarmid
 		instance.ObjectMeta.Finalizers = append(instance.ObjectMeta.Finalizers, `dockerswarms.swarm.aws.gotopple.com`)
 
 		err = r.Update(context.TODO(), instance)
 		if err != nil {
 			// If the call to update the resource annotations has failed then
-			// the Subnet resource will not be able to track the created Subnet and
+			// the DockerSwarm resource will not be able to track the created DockerSwarm and
 			// no finalizer will have been appended.
 			//
-			// This routine should attempt to delete the AWS Subnet before
+			// This routine should attempt to delete the AWS DockerSwarm before
 			// returning the error and retrying.
 
 			r.events.Eventf(instance,
@@ -174,16 +176,16 @@ func (r *ReconcileSubnet) Reconcile(request reconcile.Request) (reconcile.Result
 				"Failed to update the resource: %s", err.Error())
 
 			deleteOutput, ierr := ec2svc.DeleteSubnet(&ec2.DeleteSubnetInput{
-				SubnetId: aws.String(dockerswarmid),
+				//SubnetId: aws.String(dockerswarmid),
 			})
 			if ierr != nil {
 				// Send an appropriate event that has been annotated
 				// for async AWS resource GC.
 				r.events.AnnotatedEventf(instance,
-					map[string]string{`cleanupSubnetId`: dockerswarmid},
+					map[string]string{`cleanupDockerSwarmId`: dockerswarmid},
 					`Warning`,
 					`DeleteFailure`,
-					"Unable to delete the Subnet: %s", ierr.Error())
+					"Unable to delete the DockerSwarm: %s", ierr.Error())
 
 				if aerr, ok := ierr.(awserr.Error); ok {
 					switch aerr.Code() {
@@ -200,39 +202,17 @@ func (r *ReconcileSubnet) Reconcile(request reconcile.Request) (reconcile.Result
 				// Send an appropriate event that has been annotated
 				// for async AWS resource GC.
 				r.events.AnnotatedEventf(instance,
-					map[string]string{`cleanupSubnetId`: dockerswarmid},
+					map[string]string{`cleanupDockerSwarmId`: dockerswarmid},
 					`Warning`,
 					`DeleteAmbiguity`,
-					"Attempt to delete the Subnet recieved a nil response")
-				return reconcile.Result{}, fmt.Errorf(`DeleteSubnetOutput was nil`)
+					"Attempt to delete the DockerSwarm recieved a nil response")
+				return reconcile.Result{}, fmt.Errorf(`DeleteDockerSwarmOutput was nil`)
 			}
 			return reconcile.Result{}, err
 		}
 		r.events.Event(instance, `Normal`, `Annotated`, "Added finalizer and annotations")
 
 		// Make sure that there are tags to add before attempting to add them.
-		if len(instance.Spec.Tags) >= 1 {
-			// Tag the new Subnet
-			ts := []*ec2.Tag{}
-			for _, t := range instance.Spec.Tags {
-				ts = append(ts, &ec2.Tag{
-					Key:   aws.String(t.Key),
-					Value: aws.String(t.Value),
-				})
-			}
-			tagOutput, err := ec2svc.CreateTags(&ec2.CreateTagsInput{
-				Resources: []*string{aws.String(dockerswarmid)},
-				Tags:      ts,
-			})
-			if err != nil {
-				r.events.Eventf(instance, `Warning`, `TaggingFailure`, "Tagging failed: %s", err.Error())
-				return reconcile.Result{}, err
-			}
-			if tagOutput == nil {
-				return reconcile.Result{}, fmt.Errorf(`CreateTagsOutput was nil`)
-			}
-			r.events.Event(instance, `Normal`, `Tagged`, "Added tags")
-		}
 	} else if instance.ObjectMeta.DeletionTimestamp != nil {
 		// remove the finalizer
 		for i, f := range instance.ObjectMeta.Finalizers {
@@ -245,16 +225,16 @@ func (r *ReconcileSubnet) Reconcile(request reconcile.Request) (reconcile.Result
 
 		// must delete
 		_, err = ec2svc.DeleteSubnet(&ec2.DeleteSubnetInput{
-			SubnetId: aws.String(dockerswarmid),
+			//SubnetId: aws.String(dockerswarmid),
 		})
 		if err != nil {
-			r.events.Eventf(instance, `Warning`, `DeleteFailure`, "Unable to delete the Subnet: %s", err.Error())
+			r.events.Eventf(instance, `Warning`, `DeleteFailure`, "Unable to delete the DockerSwarm: %s", err.Error())
 
 			// Print the error, cast err to awserr.Error to get the Code and
 			// Message from an error.
 			if aerr, ok := err.(awserr.Error); ok {
 				switch aerr.Code() {
-				case `InvalidSubnetID.NotFound`:
+				case `InvalidDockerSwarmID.NotFound`:
 					// we want to keep going
 					r.events.Eventf(instance, `Normal`, `AlreadyDeleted`, "The Subnet: %s was already deleted", err.Error())
 				default:
@@ -271,7 +251,7 @@ func (r *ReconcileSubnet) Reconcile(request reconcile.Request) (reconcile.Result
 			r.events.Eventf(instance, `Warning`, `ResourceUpdateFailure`, "Unable to remove finalizer: %s", err.Error())
 			return reconcile.Result{}, err
 		}
-		r.events.Event(instance, `Normal`, `Deleted`, "Deleted Subnet and removed finalizers")
+		r.events.Event(instance, `Normal`, `Deleted`, "Deleted DockerSwarm and removed finalizers")
 	}
 
 	return reconcile.Result{}, nil
